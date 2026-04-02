5a0f7969b33d46b80751d0449aa340ef29811275
Peter Páleník <petko.palenik@gmail.com>
2026-04-02 12:59:05 +0200

    feat: sqlite nuget package added and CharacterEntity created

---
+++ DnD-InventoryManager/Models/CharacterEntity.cs
@@ -0,0 +1,14 @@
+using SQLite;
+
+namespace DnD_InventoryManager.Models;
+
+[Table("Characters")]
+public class CharacterEntity
+{
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string ImagePath { get; set; } = "dotnet_bot.png";
+    public int Strength { get; set; }
+    public int Size { get; set; }
+}
