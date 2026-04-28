289880d7588650f48334b3f5bbcc7b4665d3a8d7
Peter Páleník <petko.palenik@gmail.com>
2026-04-28 21:06:45 +0200

    feat: dice roller timer added

--- DnD-InventoryManager/ViewModels/DiceRollerViewModel.cs
+++ DnD-InventoryManager/ViewModels/DiceRollerViewModel.cs
@@ -9,6 +9,8 @@ public partial class DiceRollerViewModel : ViewModelBase
     [ObservableProperty] private int _modifier = 0;
     [ObservableProperty] private string _resultText = "-";
     [ObservableProperty] private string _calculationText = "Shake your phone";
+
+    [ObservableProperty] private bool _isRolling;
     
     public List<string> DiceOptions { get; } = new() {"d100", "d20", "d12", "d10", "d8", "d6", "d4"};
 
@@ -20,18 +22,26 @@ public partial class DiceRollerViewModel : ViewModelBase
     [RelayCommand]
     public void Roll()
     {
-        int sides = int.Parse(SelectedDice.Replace("d", ""));
-        
-        int roll = new Random().Next(1, sides + 1);
-        int total = roll + Modifier;
-        
-        ResultText = total.ToString();
-
-        string sign = Modifier >= 0 ? "+" : "";
-        CalculationText = $"({roll}) {sign} {Modifier}";
+        if (IsRolling)
+        {
+            return;
+        }
 
         try
         {
+
+            IsRolling = true;
+
+            int sides = int.Parse(SelectedDice.Replace("d", ""));
+
+            int roll = new Random().Next(1, sides + 1);
+            int total = roll + Modifier;
+
+            ResultText = total.ToString();
+
+            string sign = Modifier >= 0 ? "+" : "";
+            CalculationText = $"({roll}) {sign} {Modifier}";
+
             if (Vibration.Default.IsSupported)
             {
                 Vibration.Default.Vibrate(TimeSpan.FromMilliseconds(50));
@@ -40,6 +50,10 @@ public partial class DiceRollerViewModel : ViewModelBase
         catch
         {
             return;
+        }
+        finally
+        {
+            IsRolling = false;
         }
 
     }
