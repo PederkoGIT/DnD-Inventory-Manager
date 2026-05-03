cadfa70b1196551bb103c89dab6c61162883e83b
m-dano <martin.dano3@gmail.com>
2026-05-04 00:38:19 +0200

    feat: add QR display page and viewmodel

---
+++ DnD-InventoryManager/ViewModels/QrCodeDisplayViewModel.cs
@@ -0,0 +1,32 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using DnD_InventoryManager.Models;
+using DnD_InventoryManager.Services;
+
+namespace DnD_InventoryManager.ViewModels;
+
+[QueryProperty(nameof(Character), "Character")]
+public partial class QrCodeDisplayViewModel : ViewModelBase
+{
+    private readonly QrService _qrService;
+    
+    [ObservableProperty]
+    private string qrPayload = string.Empty;
+
+    [ObservableProperty]
+    private Character? character;
+
+    public QrCodeDisplayViewModel(QrService qrService)
+    {
+        _qrService = qrService;
+        Title = "Zdieľať cez QR";
+    }
+    
+    partial void OnCharacterChanged(Character? value)
+    {
+        if (value != null)
+        {
+            Title = $"QR: {value.Name}";
+            QrPayload = _qrService.EncodeCharacter(value);
+        }
+    }
+}
