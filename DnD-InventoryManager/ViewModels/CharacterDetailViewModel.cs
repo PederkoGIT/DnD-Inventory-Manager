103b5442f08b2c1991e326ae78f4ae181b8c1c54
TurbinaXY <danielputis@gmail.com>
2026-05-07 18:41:52 +0200

    filtering by category in CharacterDetail

--- DnD-InventoryManager/ViewModels/CharacterDetailViewModel.cs
+++ DnD-InventoryManager/ViewModels/CharacterDetailViewModel.cs
@@ -15,20 +15,22 @@ public partial class CharacterDetailViewModel(
     NfcService nfcService
 ) : ViewModelBase
 {
+    private const string NoFilter = "No Filter";
+    
+    private ICollection<ItemModel> _allItems = [];
+
     [ObservableProperty]
     public partial CharacterModel? Character { get; set; }
-
     [ObservableProperty]
     public partial bool IsWaitingForNfc { get; set; }
-
     [ObservableProperty]
     public partial ObservableCollection<ItemModel> Items { get; set; } = [];
-    
-    private List<ItemModel> _allItems = [];
-    
     [ObservableProperty]
     public partial string SearchQuery { get; set; } = string.Empty;
-    
+    [ObservableProperty] 
+    public partial IList<string> AllCategories { get; set; } = [];
+    [ObservableProperty] 
+    public partial string CategoryFilter { get; set; } = NoFilter;
     [ObservableProperty]
     public partial double CurrentLoad { get; set; }
     [ObservableProperty]
@@ -49,9 +51,10 @@ public partial class CharacterDetailViewModel(
             Character = updatedCharacter;
             Title = Character.Name;
             
-            var items = await itemFacade.GetAllByCharacterIdAsync(updatedCharacter.Id);
-            _allItems = items.ToList();
+            _allItems = await itemFacade.GetAllByCharacterIdAsync(updatedCharacter.Id);
+            AllCategories = [NoFilter, .. _allItems.Select(i => i.Category).Distinct().ToList()];
             
+
             CurrentLoad = _allItems.Sum(i => i.Weight * i.Quantity);
             CurrentLoadPercentage = Character.CarryingCapacity > 0 ? CurrentLoad / Character.CarryingCapacity : 0;
             
@@ -72,17 +75,30 @@ public partial class CharacterDetailViewModel(
         FilterItems();
     }
 
+    partial void OnCategoryFilterChanged(string value)
+    {
+        FilterItems();
+    }
+    
     private void FilterItems()
     {
-        if (string.IsNullOrEmpty(SearchQuery))
+        IEnumerable<ItemModel> filtered;
+
+        if (string.IsNullOrWhiteSpace(CategoryFilter) || CategoryFilter.Equals(NoFilter))
         {
-            Items = new ObservableCollection<ItemModel>(_allItems);
+            filtered = _allItems;
         }
         else
         {
-            var filtered = _allItems.Where(i => i.Name.ToLower().Contains(SearchQuery.ToLower()));
-            Items = new ObservableCollection<ItemModel>(filtered);
+            filtered = _allItems.Where(i => i.Category.Contains(CategoryFilter, StringComparison.CurrentCultureIgnoreCase));
         }
+
+        if (!string.IsNullOrEmpty(SearchQuery))
+        {
+            filtered = filtered.Where(i => i.Name.Contains(SearchQuery, StringComparison.CurrentCultureIgnoreCase));
+        }
+
+        Items = new ObservableCollection<ItemModel>(filtered);
     }
 
     [RelayCommand]
